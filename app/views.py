8403903dee264bff163aa9e479d233284da56ce6
AdamTaylor9 <tayloradamross@gmail.com>
2024-02-29 00:05:37 -0500

    Lab-3 Submission

--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from app import mail
+from flask_mail import Message
+from .forms import ContactForm
 
 
 ###
@@ -16,6 +19,26 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route('/contact', methods=['POST', 'GET'])
+def contact():
+    form = ContactForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            name = form.name.data
+            email = form.email.data
+            subject = form.subject.data
+            message = form.message.data
+
+            msg = Message(subject, sender=(name, email), recipients=["addyta2002@gmail.com"])
+            msg.body = message
+            mail.send(msg)
+            flash('Email delivered', 'success')
+            return redirect(url_for('home'))
+        
+        flash_errors(form)
+
+    return render_template('contact.html', form=form)
 
 
 ###
